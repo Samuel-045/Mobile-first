912104af9b7d21213c5ff0ab0e0bea68487aeaf6
Samuel-045 <samuelvic856@gmail.com>
2023-08-11 19:09:30 -0300

    Criação da verificação do form do aside

--- js/vida.js
+++ js/vida.js
@@ -41,21 +41,44 @@ const nomeAside = document.getElementById("nome2")
 const emailAside = document.getElementById("email2")
 
 function verificacaoAside(){
+    var nomeCond = false
     const nome = nomeAside.value.trim()
     const email = emailAside.value.trim()
 
-    if(nome==='' || email==='' || nome.lenght<2){
+   
+
+    if(nome.length<=1){
+        document.getElementById("erroAside2").innerHTML="Nome muito curto"
+        nomeCond = nomeCond
+    }else{
+        nomeCond =! nomeCond
+        document.getElementById("erroAside2").innerHTML="Nome válido"
+    }
+
+    const regex = /^[a-z0-9]+@[a-z0-9.-]+\.[a-z]{2,}$/i
+    var cond =regex.test(email)
+
+    if(cond){
+        document.getElementById("erroAside3").innerHTML="Email válido"
+    }else{
+        document.getElementById("erroAside3").innerHTML="Email inválido<br><br>"
+    }
+
+    if(nome==='' || email==='' || nome.length<=1 || cond===false){
         document.getElementById("erroAside1").innerHTML="Preencha os campos corretamente!!</br>";   
+    }else if(cond && nomeCond){
+        document.getElementById("erroAside1").innerHTML="Cadastro realizado com sucesso "+nomeAside.value+" !!"  
     }else{
         document.getElementById("erroAside1").innerHTML="";   
     }
+
 }
 
 document.querySelector("#submit2").addEventListener('click', event => {
     event.preventDefault()
     verificacaoAside() 
     
-    console.log("teste")
+    
 })
 
-
+//3ª parte
